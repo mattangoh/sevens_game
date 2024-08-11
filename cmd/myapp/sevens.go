b0fbf6b169fbb1a800bb2b1e562f15e0baf26424
mattangoh <mattangoh@gmail.com>
2024-08-11 13:53:48 -0700

    added UserInput Function
    Signed-off-by: mattangoh <mattangoh@gmail.com>

--- cmd/myapp/sevens.go
+++ cmd/myapp/sevens.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 type Rolls struct {
@@ -10,12 +11,32 @@ type Rolls struct {
     sum int
   }
 
+type User struct {
+  betAmount float64
+  guess string
+}
+
 func main() {
   
   fmt.Println("**********")
   fmt.Println("Welcome to the Sevens Game!")
   fmt.Println("**********")
+  
+  // User inputs
+  user := UserInput()
+  fmt.Println("User Input Summary:")
+  fmt.Printf("You bet $%v\n", user.betAmount)
+  fmt.Printf("You guessed %v.\n", user.guess)
+  
 
+  // Dealer rolling interaction
+  fmt.Println("Dealer is Rolling")
+
+  for i := 0; i < 3; i++ {
+   time.Sleep(1 * time.Second)
+   fmt.Println(".")
+  } 
+  
   dealer := DealerRoll()
 
   fmt.Printf("The first value is %v. The second value is %v, the sum is %v",
@@ -31,4 +52,46 @@ func DealerRoll() Rolls {
   dealer := Rolls{roll1, roll2, sum}
   
   return dealer
+}
+
+func UserInput() User {
+  var betAmount float64
+
+  for {
+    fmt.Println("Please enter your bet amount!")
+    _, err := fmt.Scanln(&betAmount)
+    
+    if err != nil {
+      fmt.Println("Invalid input! Please re-enter a valid number for your float")
+      // In case of extra lines
+      var discard string
+      fmt.Scanln(&discard)
+    } else if betAmount < 0 {
+        fmt.Println("Bet amount must be greater than $0")
+    } else {
+        break
+    }
+  }
+   
+  var guess string
+
+  for {
+    fmt.Println("What is your guess (over, under, sevens)")
+    _, err := fmt.Scanln(&guess)
+
+    if err != nil {
+      fmt.Println("Invalid input for guess. Please input a valid string.")
+      // In case of extra lines
+      var discard string
+      fmt.Scanln(&discard)
+    } else if guess != "over" && guess != "under" && guess != "sevens" {
+      fmt.Println("Invalid guess. Must be either 'over', 'under', or 'sevens'.")
+    } else {
+        break
+    }
+  }
+  
+  user := User {betAmount, guess}
+
+  return user
 }
